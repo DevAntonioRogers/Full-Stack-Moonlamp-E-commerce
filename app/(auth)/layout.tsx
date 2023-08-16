3d6605c1ee6d095a46b227b6ba128993fa6982fb
DevAntonioRogers <ar919061493@gmail.com>
2023-08-16 19:08:41 -0400

    Clerk

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
+  style: ["normal", "italic"],
+});
+
+export const metadata: Metadata = {
+  title: "Moonlamp",
+  description: "Shop cool moonlamps",
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className={poppins.className}>
+        <Hydration>
+          <Navbar />
+          {children}
+        </Hydration>
+      </body>
+    </html>
+  );
+}
